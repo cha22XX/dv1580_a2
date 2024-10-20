60c1cda12bbb64b7de33538c5bb33e0dd63ef0d3
cha22XX <129762303+cha22XX@users.noreply.github.com>
2024-10-20 13:38:21 +0200

    Update linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -1,5 +1,5 @@
 // linked_list.h
-#ifndef LINKED_LIST_H
+/*#ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
 #include "memory_manager.h" // Include your custom memory manager
@@ -13,7 +13,7 @@ typedef struct Node
     pthread_mutex_t lock;
 
 } Node;*/
-
+/*
 typedef struct Node {
     int data;
     struct Node* next;
@@ -29,7 +29,38 @@ void list_delete(Node **head, uint16_t data);
 Node *list_search(Node **head, uint16_t data);
 
 void list_display(Node **head);
-void list_display_range(Node **head, Node *start_node, Node *end_node);
+void list_display_range(Node **head, Node *start_node, Node *end_node);*/
+
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <pthread.h>
+
+// 
+typedef struct Node {
+    int data;
+    struct Node* next;
+    pthread_mutex_t node_mutex; 
+} Node;
+
+void list_init(Node** head, size_t size);               
+void list_insert(Node** head, uint16_t data);                  
+void list_insert_after(Node* prev_node, uint16_t data);        
+void list_insert_before(Node** head, Node* next_node, uint16_t data);  
+void list_delete(Node** head, uint16_t data);                  
+Node* list_search(Node** head, uint16_t data);                
+void list_display(Node** head);                         
+void list_display_range(Node** head, Node* start_node, Node* end_node); 
+int list_count_nodes(Node** head);                        
+void list_cleanup(Node** head);                          
+
+#endif
 
 int list_count_nodes(Node **head);
 void list_cleanup(Node **head);
