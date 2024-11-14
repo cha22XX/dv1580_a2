52b5b3af449bdc45aa76989c7817091bd7cde40e
cha22XX <129762303+cha22XX@users.noreply.github.com>
2024-11-14 18:51:33 +0100

    Update memory_manager.h

--- memory_manager.h
+++ memory_manager.h
@@ -1,23 +1,30 @@
-#ifndef MEMORY_MANAGER_H  
-#define MEMORY_MANAGER_H  
+#ifndef MEMORY_MANAGER_H
+#define MEMORY_MANAGER_H
+
+#include <stddef.h> // For size_t
 
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
-#include <stddef.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
+
+// Helps C++ compilers to handle C header files
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
 #define POOL_SIZE 81920000 
 #define MAX_BLOCKS 100000      
 
 
 typedef struct Block {
-    void* address; 
-    size_t size;    
-    bool is_free;   
-    struct Block* next; 
+    void* address; // 
+    size_t size;   //
+    bool is_free;  // 
+    struct Block* next; // 
 } Block;
 
 
@@ -28,6 +35,8 @@ void* mem_alloc(size_t size);
 void mem_free(void* block);
 void* mem_resize(void* block, size_t size);
 void mem_deinit(void);
+#ifdef __cplusplus
+}
+#endif
 
-#endif 
-
+#endif // MEMORY_MANAGER_H
