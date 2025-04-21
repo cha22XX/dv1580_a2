083ea0b48c92674b20cc96be8e58b1638479b943
cha22XX <129762303+cha22XX@users.noreply.github.com>
2025-04-21 12:29:26 +0200

    Update linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -1,38 +1,3 @@
-/*#ifndef LINKED_LIST_H
-#define LINKED_LIST_H
-
-#include <stdint.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include <pthread.h>
-
-
-#include "memory_manager.h"
-
-// 
-typedef struct Node {
-    int data;
-    struct Node* next;
-    pthread_mutex_t lock; // 
-} Node;
-
-void list_init(Node** head, size_t size);               
-void list_insert(Node** head, uint16_t data);                  
-void list_insert_after(Node* prev_node, uint16_t data);        
-void list_insert_before(Node** head, Node* next_node, uint16_t data);  
-void list_delete(Node** head, uint16_t data);                  
-Node* list_search(Node** head, uint16_t data);                
-void list_display(Node** head);                         
-void list_display_range(Node** head, Node* start_node, Node* end_node); 
-int list_count_nodes(Node** head);                        
-void list_cleanup(Node** head);                          
-
-#endif
-*/
-/*
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
@@ -44,45 +9,7 @@ void list_cleanup(Node** head);
 #include <string.h>
 #include <pthread.h>
 
-
 #include "memory_manager.h"
-
-// 
-typedef struct Node {
-    int data;
-    struct Node* next;
-    pthread_mutex_t lock; // 
-} Node;
-
-void list_init(Node** head, size_t size);               
-void list_insert(Node** head, uint16_t data);                  
-void list_insert_after(Node* prev_node, uint16_t data);        
-void list_insert_before(Node** head, Node* next_node, uint16_t data);  
-void list_delete(Node** head, uint16_t data);                  
-Node* list_search(Node** head, uint16_t data);                
-void list_display(Node** head);                         
-void list_display_range(Node** head, Node* start_node, Node* end_node); 
-int list_count_nodes(Node** head);                        
-void list_cleanup(Node** head);                          
-
-#endif*/
-
-
-
-#ifndef LINKED_LIST_H
-#define LINKED_LIST_H
-
-#include <stdint.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include <pthread.h>
-
-
-#include "memory_manager.h"
-
 
 // Definition of a singly linked list node.
 typedef struct Node {
@@ -91,7 +18,6 @@ typedef struct Node {
     pthread_mutex_t lock; 
 } Node;
 
-
 void list_init(Node** head, size_t size);               
 void list_insert(Node** head, uint16_t data);                  
 void list_insert_after(Node* prev_node, uint16_t data);        
@@ -104,4 +30,3 @@ int list_count_nodes(Node** head);
 void list_cleanup(Node** head);                          
 
 #endif
-
