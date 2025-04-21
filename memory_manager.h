821113adc36028d31dc3f9921ff4b5f83392e3da
cha22XX <129762303+cha22XX@users.noreply.github.com>
2025-04-21 12:32:39 +0200

    Update memory_manager.h

--- memory_manager.h
+++ memory_manager.h
@@ -1,48 +1,3 @@
-/*#ifndef MEMORY_MANAGER_H 
-#define MEMORY_MANAGER_H
-
-#include <stddef.h> // For size_t
-
-#include <stdio.h>
-#include <string.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-
-// Helps C++ compilers to handle C header files
-/*#ifdef __cplusplus
-extern "C"
-{
-#endif*/
-
-/*
-#define POOL_SIZE 81920000 
-#define MAX_BLOCKS 100000      
-
-
-typedef struct Block {
-    void* address; // 
-    size_t size;   //
-    bool is_free;  // 
-    struct Block* next; // 
-} Block;
-
-
-
-
-void mem_init(size_t size);
-void* mem_alloc(size_t size);
-void mem_free(void* block);
-void* mem_resize(void* block, size_t size);
-void mem_deinit(void);
-#ifdef __cplusplus
-}
-#endif
-
-#endif // MEMORY_MANAGER_H*/
-
-
 #ifndef MEMORY_MANAGER_H
 #define MEMORY_MANAGER_H
 
@@ -54,7 +9,6 @@ void mem_deinit(void);
 #include <stdbool.h>
 #include <stdlib.h>
 
-
 // Helps C++ compilers to handle C header files
 #ifdef __cplusplus
 extern "C"
@@ -64,16 +18,12 @@ extern "C"
 #define POOL_SIZE 81920000 
 #define MAX_BLOCKS 100000      
 
-
 typedef struct Block {
     void* address; //  
     size_t size;   // 
     bool is_free;  // 
     struct Block* next; //  
 } Block;
-
-
-
 
 void mem_init(size_t size);
 void* mem_alloc(size_t size);
@@ -82,7 +32,7 @@ void* mem_resize(void* block, size_t size);
 void mem_deinit(void);
 #ifdef __cplusplus
 }
+
 #endif
 
 #endif // MEMORY_MANAGER_H
-
